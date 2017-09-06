0ae4009f1ae5a9a4b1e02be7c64392244f7681db
Charlie Kolb <charlie.kolb@outlook.de>
2017-09-06 21:17:18 +0200

    Added LinkedList merge function Expanded Test functions

--- List.py
+++ List.py
@@ -1,5 +1,7 @@
 class SinglyNode:
     def __init__(self, data, next_node=None):
+        if data is None:
+            raise ValueError("SinglyLinkedList.__init__(): data is None!")
         self.data = data
         self.next_node = next_node
         self.length = 1 if next_node is None else next_node.length + 1
@@ -12,6 +14,8 @@ class SinglyNode:
         return self.next_node.at(index-1)
 
     def insert(self, data, index):
+        if data is None:
+            raise ValueError("SinglyLinkedList.insert(): data is None!")
         if index > self.length:
             raise IndexError("IndexOutOfBounds!")
         if index == 0:
@@ -22,7 +26,7 @@ class SinglyNode:
             self.next_node = SinglyNode(data, self.next_node)
         else:
             self.next_node.insert(data, index - 1)
-        self.length += + 1
+        self.length += 1
 
     # call recursively until we find data, if found rotate out deleted data to last node and return true,
     # signaling successful removal, so that other nodes decrease length accordingly
@@ -46,8 +50,28 @@ class SinglyNode:
             else:
                 return False
 
+    # merges the two lists and returns the new head
+    def merge(self, otherHead, index=0):
+        if otherHead is None:
+            return self
+        if index < 0 or index > self.length:
+            raise IndexError('SinglyNode.merge: index too low/high!')
+        if index == 0:
+            otherHead.length += self.length
+            otherHead.insert(self, otherHead.length)
+            return otherHead
+        elif self.next_node is None:
+            self.length += otherHead.length
+            self.next_node = otherHead
+            return self
+        else:
+            self.next_node.merge(otherHead, index-1)
+            return self
+
+
 
 class SinglyLinkedList:
+    # ToDo: Add a kind of initializer_list? maybe that *args / **kwargs thingy?
     def __init__(self):
         self.head = None
 
@@ -78,3 +102,16 @@ class SinglyLinkedList:
             self.head = SinglyNode(data)
         else:
             self.add(data, self.head.length)
+
+    def merge(self, other, index=0):
+        if other is None:
+            return
+        if self.head is None:
+            if index != 0:
+                raise IndexError('SinglyLinkedList.merge(): This list was previously empty, and merge index was not 0!')
+            self.head = other.head
+        elif index < 0 or index > self.head.length:
+            raise IndexError('SinglyLinkedList.merge(): index too low/high!')
+        else:
+            self.head = self.head.merge(other.head)
+
--- Tester.py
+++ Tester.py
@@ -2,6 +2,8 @@ import random
 import unittest
 import List
 
+TestClass = List.SinglyLinkedList
+
 
 def random_list(lower_bound=0, upper_bound=100, size=10):
     return random.sample(range(lower_bound, upper_bound), size)
@@ -10,7 +12,7 @@ def random_list(lower_bound=0, upper_bound=100, size=10):
 class TestAddMethods(unittest.TestCase):
     def test_add_last(self):
         data = random_list()
-        test_struct = List.SinglyLinkedList()
+        test_struct = TestClass()
         test_data = []
         for i in range(len(data)):
             test_data.append(data[i])
@@ -18,6 +20,25 @@ class TestAddMethods(unittest.TestCase):
             print("{0} vs {1}".format(test_data[i], test_struct.at(i)))
             self.assertEqual(test_data[i], test_struct.at(i))
 
+    def test_add_first(self):
+        data = random_list()
+        test_struct = TestClass()
+        test_data = []
+        for i in range(len(data)):
+            test_data.insert(0, data[i])
+            test_struct.add_front(data[i])
+            print("{0} vs {1}".format(test_data[0], test_struct.at(0)))
+            self.assertEqual(test_data[0], test_struct.at(0))
+
+    def test_add_random_index(self):
+        data = random_list()
+        test_struct = TestClass()
+        test_data = []
+        for i in range(len(data)):
+            test_data.insert(0, data[i])
+            test_struct.add_front(data[i])
+            print("{0} vs {1}".format(test_data[0], test_struct.at(0)))
+            self.assertEqual(test_data[0], test_struct.at(0))
 
 if __name__ == '__main__':
     unittest.main()
